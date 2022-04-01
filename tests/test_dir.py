3e5f8803c5c30108fcf7c1837f2b41a8ccd6fb24
DavdGao <102287034+DavdGao@users.noreply.github.com>
2022-04-01 16:37:18 +0800

    Update test_dir.py

--- tests/test_dir.py
+++ tests/test_dir.py
@@ -7,7 +7,7 @@ class DIRTest(unittest.TestCase):
         print(('Testing %s.%s' % (type(self).__name__, self._testMethodName)))
 
     def test_dir(self):
-        self.assertTrue(os.path.exists('tests/test_dir.py'))
+        self.assertTrue(os.path.exists('tests/test_dir1.py'))
 
 
 
