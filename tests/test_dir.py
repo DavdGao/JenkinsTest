41fc60f3200b91b045d93f565d3748d7a62280bb
DavdGao <102287034+DavdGao@users.noreply.github.com>
2022-04-01 15:32:13 +0800

    Update test_dir.py

--- tests/test_dir.py
+++ tests/test_dir.py
@@ -7,7 +7,7 @@ class MFTest(unittest.TestCase):
         print(('Testing %s.%s' % (type(self).__name__, self._testMethodName)))
 
     def test_dir(self):
-        self.assertTrue(os.path.exists('tests/test_abc.py'))
+        self.assertTrue(os.path.exists('tests/test_dir.py'))
 
 
 
